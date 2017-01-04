dcb00c8cb63828f14ee91a78acd25c245925dffb
Garrett Cullen <garrettwcullen@gmail.com>
2017-01-03 16:33:37 -0800

    push

--- wp-content/themes/hartman/page-main.php
+++ wp-content/themes/hartman/page-main.php
@@ -7,7 +7,56 @@
 
 		
 
-			<?php  // get_template_part( 'loop', 'page' );?>
+<div class="main_banner">
+	
+	<div class="main_inner_banner">
+	
+		<div class="welcome_content">
+		
+			<span class="subheader">the attorney you choose</span><!-- subheader -->
+			<span class="largeheader">Does Make<br/>a Difference</span>
+			<a href="" class="free_consultation">Schedule your free consultation</a>
+		
+		</div><!-- welcome_content -->
+		
+		<div class="lower_content">
+		
+		<div class="icon_wrapper">
+			
+			
+			<div class="single_icon">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/header_icon_v2_sp1.svg"/>
+				
+			</div><!-- single_icon -->
+			
+			<div class="single_icon">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/header_icon_v2_sp2.svg"/>
+				
+			</div><!-- single_icon -->
+			
+			<div class="single_icon">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/header_icon_v2_sp3.svg"/>
+				
+			</div><!-- single_icon -->
+			
+		</div><!-- icon_wrapper -->
+		
+		<div class="video_wrapper">
+			
+			
+		</div><!-- video_wrapper -->
+		
+	</div><!-- lower_content -->
+	
+	</div><!-- main_inner_banner -->
+	
+	
+	
+	
+</div><!-- main_banner -->
 
 			
 
