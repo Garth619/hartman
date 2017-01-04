ec6d2f2730ac3b247909353b50dd1c6aa153179d
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-01-03 20:09:41 -0800

    Push

--- wp-content/themes/hartman/page-main.php
+++ wp-content/themes/hartman/page-main.php
@@ -27,24 +27,39 @@
 			<div class="single_icon">
 				
 				<img src="<?php bloginfo('template_directory');?>/images/header_icon_v2_sp1.svg"/>
+				<span class="largeheader">Record Setting</span><!-- largeheader -->
+				<span class="subheader">Verdicts</span><!-- subheader -->
 				
 			</div><!-- single_icon -->
 			
 			<div class="single_icon">
 				
 				<img src="<?php bloginfo('template_directory');?>/images/header_icon_v2_sp2.svg"/>
+				<span class="largeheader">Winner Of</span><!-- largeheader -->
+				<span class="subheader">Numerous Awards</span><!-- subheader -->
 				
 			</div><!-- single_icon -->
 			
 			<div class="single_icon">
 				
 				<img src="<?php bloginfo('template_directory');?>/images/header_icon_v2_sp3.svg"/>
+				<span class="largeheader">Dallas Gives Back</span><!-- largeheader -->
+				<span class="subheader">To the Community</span><!-- subheader -->
 				
 			</div><!-- single_icon -->
 			
 		</div><!-- icon_wrapper -->
 		
 		<div class="video_wrapper">
+			
+			<div class="video_thumb">
+				
+				<img class="video_thumb_img" src="<?php bloginfo('template_directory');?>/images/mobile_header_video_thumb.jpg"/>
+				<img class="play" src="<?php bloginfo('template_directory');?>/images/header_video_playbutton.png"/>
+				
+			</div><!-- video_thumb -->
+			
+			<div class="video_content"></div><!-- video_content -->
 			
 			
 		</div><!-- video_wrapper -->
