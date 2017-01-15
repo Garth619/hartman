891d7a9775398d8a094af90284fac5465bcd5aae
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-01-15 11:00:54 -0800

    push

---
+++ wp-content/themes/hartman/page-bio.php
@@ -0,0 +1,77 @@
+<?php 
+	
+/**
+ * Template Name: Attorney Bio
+ *
+ 
+ */
+	
+	get_header(); ?>
+
+	<?php include('banner.php');?>
+	
+
+		<div class="inner_main_no_sidebar">
+			
+			<div class="inner_container">
+				
+				
+				<div class="content">
+					
+					
+					<div class="sidebar_bio">
+					
+					
+						<table class="blue_header_wrapper">
+					
+							<tr>
+								
+								<td class="header_blue_line"></td><!-- blue_line -->
+								
+								<td>
+									<img src="<?php bloginfo('template_directory');?>/images/internal_bio_hartman.jpg"/>
+									<a class="email_attorney" href="">Email Dallas</a><!-- email_attorney -->
+								</td>
+						
+							</tr>
+					
+						</table><!-- blue_header_wrapper -->
+					
+					
+					
+					</div><!-- sidebar_bio -->
+					
+					<div class="inner_content">
+						
+					<h1>Dallas W. Hartman</h1>
+					
+					<span class="my_excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</span>
+					
+					
+					
+					<h2>Lorem ipsum dolor sit amet</h2>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam.</p>
+					
+					
+					
+						<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat.</p>
+
+						<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde</p> 
+					
+					
+					
+				</div><!-- content -->
+				
+				
+				
+				</div><!-- inner_content -->
+				
+				
+			</div><!-- inner_container -->
+			
+			
+		</div><!-- inner_main_with_sidebar -->
+
+	
+	<?php get_footer(); ?>
