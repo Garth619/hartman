50080733ef148310db252e9a4b40115d56137bcd
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-01-15 15:17:43 -0800

    push

---
+++ wp-content/themes/hartman/page-attorneysdirectory.php
@@ -0,0 +1,226 @@
+<?php 
+	
+/**
+ * Template Name: Attorneys Directory
+ *
+ 
+ */
+	
+	get_header(); ?>
+
+	<?php include('banner.php');?>
+	
+
+		<div class="inner_main_no_sidebar">
+			
+			<div class="inner_container">
+				
+				
+				<div class="content">
+					
+					
+					<table class="blue_header_wrapper">
+					
+					<tr>
+						<td class="header_blue_line"></td><!-- blue_line -->
+						<td><h1><?php the_title();?></h1></td>
+						
+						
+					</tr>
+					
+					</table><!-- header_wrapper -->
+					
+					<div class="inner_content">
+					
+					<span class="my_excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</span>
+					
+					<div class="col">
+					
+					
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis  es so nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  Du Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id. </p>
+					
+					</div><!-- col -->
+					
+					<div class="col">
+					
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis  es so nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  Du Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+					
+					</div><!-- col -->
+					
+				</div><!-- content -->
+				
+				<div class="top_attorneys_wrapper">
+					
+					<a class="single_attorney_link" href="">
+						
+						<div class="single_attorney top">
+							
+							<div class="image_wrapper">
+								
+								<img src="<?php bloginfo('template_directory');?>/images/internal_profilepic_hartman.jpg"/>
+								<span class="attorney_name">Dallas W. Hartman</span><!-- attorney_name -->
+								
+							</div><!-- image_wrapper -->
+							
+						</div><!-- single_attorney -->
+						
+					</a><!-- single_attorney_link -->
+					
+					<a class="single_attorney_link" href="">
+						
+						<div class="single_attorney top">
+							
+							<div class="image_wrapper">
+								
+								<img src="<?php bloginfo('template_directory');?>/images/internal_profilepic_hartman.jpg"/>
+								<span class="attorney_name">Dallas W. Hartman</span><!-- attorney_name -->
+								
+							</div><!-- image_wrapper -->
+							
+						</div><!-- single_attorney -->
+						
+					</a><!-- single_attorney_link -->
+					
+				</div><!-- top_attorneys_wrapper -->
+				
+				<div class="all_attorneys_wrapper">
+					
+					
+					<a class="single_attorney_link" href="">
+						
+						<div class="single_attorney bottom">
+							
+							<div class="image_wrapper">
+								
+								<img src="<?php bloginfo('template_directory');?>/images/internal_profilepic_hartman.jpg"/>
+								<span class="attorney_name">Dallas W. Hartman</span><!-- attorney_name -->
+								
+							</div><!-- image_wrapper -->
+							
+						</div><!-- single_attorney -->
+						
+					</a><!-- single_attorney_link -->
+					
+					<a class="single_attorney_link" href="">
+						
+						<div class="single_attorney bottom">
+							
+							<div class="image_wrapper">
+								
+								<img src="<?php bloginfo('template_directory');?>/images/internal_profilepic_hartman.jpg"/>
+								<span class="attorney_name">Dallas W. Hartman</span><!-- attorney_name -->
+								
+							</div><!-- image_wrapper -->
+							
+						</div><!-- single_attorney -->
+						
+					</a><!-- single_attorney_link -->
+					
+					<a class="single_attorney_link" href="">
+						
+						<div class="single_attorney bottom">
+							
+							<div class="image_wrapper">
+								
+								<img src="<?php bloginfo('template_directory');?>/images/internal_profilepic_hartman.jpg"/>
+								<span class="attorney_name">Dallas W. Hartman</span><!-- attorney_name -->
+								
+							</div><!-- image_wrapper -->
+							
+						</div><!-- single_attorney -->
+						
+					</a><!-- single_attorney_link -->
+					
+					<a class="single_attorney_link" href="">
+						
+						<div class="single_attorney bottom">
+							
+							<div class="image_wrapper">
+								
+								<img src="<?php bloginfo('template_directory');?>/images/internal_profilepic_hartman.jpg"/>
+								<span class="attorney_name">Dallas W. Hartman</span><!-- attorney_name -->
+								
+							</div><!-- image_wrapper -->
+							
+						</div><!-- single_attorney -->
+						
+					</a><!-- single_attorney_link -->
+					
+					<a class="single_attorney_link" href="">
+						
+						<div class="single_attorney bottom">
+							
+							<div class="image_wrapper">
+								
+								<img src="<?php bloginfo('template_directory');?>/images/internal_profilepic_hartman.jpg"/>
+								<span class="attorney_name">Dallas W. Hartman</span><!-- attorney_name -->
+								
+							</div><!-- image_wrapper -->
+							
+						</div><!-- single_attorney -->
+						
+					</a><!-- single_attorney_link -->
+					
+					<a class="single_attorney_link" href="">
+						
+						<div class="single_attorney bottom">
+							
+							<div class="image_wrapper">
+								
+								<img src="<?php bloginfo('template_directory');?>/images/internal_profilepic_hartman.jpg"/>
+								<span class="attorney_name">Dallas W. Hartman</span><!-- attorney_name -->
+								
+							</div><!-- image_wrapper -->
+							
+						</div><!-- single_attorney -->
+						
+					</a><!-- single_attorney_link -->
+					
+					<a class="single_attorney_link" href="">
+						
+						<div class="single_attorney bottom">
+							
+							<div class="image_wrapper">
+								
+								<img src="<?php bloginfo('template_directory');?>/images/internal_profilepic_hartman.jpg"/>
+								<span class="attorney_name">Dallas W. Hartman</span><!-- attorney_name -->
+								
+							</div><!-- image_wrapper -->
+							
+						</div><!-- single_attorney -->
+						
+					</a><!-- single_attorney_link -->
+					
+					<a class="single_attorney_link" href="">
+						
+						<div class="single_attorney bottom">
+							
+							<div class="image_wrapper">
+								
+								<img src="<?php bloginfo('template_directory');?>/images/internal_profilepic_hartman.jpg"/>
+								<span class="attorney_name">Dallas W. Hartman</span><!-- attorney_name -->
+								
+							</div><!-- image_wrapper -->
+							
+						</div><!-- single_attorney -->
+						
+					</a><!-- single_attorney_link -->
+					
+					
+				</div><!-- all_attorneys_wrapper -->
+				
+								
+				</div><!-- inner_content -->
+				
+				
+			</div><!-- inner_container -->
+			
+			
+		</div><!-- inner_main_with_sidebar -->
+
+			<?php  // get_template_part( 'loop', 'page' );?>
+
+			
+
+<?php get_footer(); ?>
