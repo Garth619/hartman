6c6515956f95cf8a529ae667b4b3b5843f837214
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-01-14 22:31:16 -0800

    push

---
+++ wp-content/themes/hartman/page-directory.php
@@ -0,0 +1,64 @@
+<?php 
+	
+/**
+ * Template Name: Practice Areas Directory
+ *
+ 
+ */
+	
+	get_header(); ?>
+
+	<?php include('banner.php');?>
+	
+
+		<div class="inner_main_no_sidebar">
+			
+			<div class="inner_container">
+				
+				
+				<div class="content">
+					
+					
+					<table class="blue_header_wrapper">
+					
+					<tr>
+						<td class="header_blue_line"></td><!-- blue_line -->
+						<td><h1><?php the_title();?></h1></td>
+					</tr>
+					
+					</table><!-- header_wrapper -->
+					
+					<div class="inner_content">
+					
+						<ul class="grid">
+							
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							<li><a href="">Practice Areas</a></li>
+							
+						</ul><!-- grid -->
+													
+					</div><!-- inner_content -->
+				
+				</div><!-- content -->
+				
+				
+			</div><!-- inner_container -->
+			
+			
+		</div><!-- inner_main_with_sidebar -->
+
+			<?php  // get_template_part( 'loop', 'page' );?>
+
+			
+
+<?php get_footer(); ?>
