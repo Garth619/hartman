a0a69cf703309d7e5cab634ea51c5e744ec4f3f1
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-01-14 21:54:12 -0800

    Push

---
+++ wp-content/themes/hartman/page-faq.php
@@ -0,0 +1,57 @@
+
+<?php 
+/**
+ *  Template Name: FAQ
+ *
+
+ *
+
+ */
+	
+	get_header(); ?>
+
+	<?php include('banner.php');?>
+	
+
+		<div class="inner_main_with_sidebar">
+			
+						
+			<?php include('sidebar.php');?>
+			
+			<div class="inner_container">
+				
+				
+				<div class="content">
+					
+					<h1>FAQ<span class="s">s</span></h1>
+					
+					<h2>i have a question that needs answering?</h2>
+					
+					<p>Here is the answer for said question. Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae</p>
+					
+					<h2>i have a question that needs answering?</h2>
+					
+					<p>Here is the answer for said question. Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae</p>
+					
+					<h2>i have a question that needs answering?</h2>
+					
+					<p>Here is the answer for said question. Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae</p>
+					
+					<h2>i have a question that needs answering?</h2>
+					
+					<p>Here is the answer for said question. Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae</p>
+
+					
+				</div><!-- content -->
+				
+				
+			</div><!-- inner_container -->
+			
+			
+		</div><!-- inner_main_with_sidebar -->
+
+			<?php  // get_template_part( 'loop', 'page' );?>
+
+			
+
+<?php get_footer(); ?>
