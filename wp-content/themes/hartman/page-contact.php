aac7f3f83f424eabf885b52843f55cf4e307bf82
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-01-14 21:42:40 -0800

    push

--- wp-content/themes/hartman/page-contact.php
+++ wp-content/themes/hartman/page-contact.php
@@ -45,6 +45,117 @@
 									
 								</div><!-- contact_info -->
 								
+								<div class="map">
+									
+									<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3007.5941665333276!2d-80.36567508458377!3d41.07786287929372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8833917f98c16d6b%3A0x81998c5d4c52c575!2s201+Green+Ridge+Rd%2C+New+Castle%2C+PA+16105!5e0!3m2!1sen!2sus!4v1484457883709" frameborder="0" style="border:0" allowfullscreen></iframe>
+								
+								</div><!-- map -->
+								
+							</div><!-- single_contact -->
+							
+							<div class="single_contact">
+								
+								<div class="contact_info">
+									
+									<span class="large_header">New Castle</span><!-- large_header -->
+									<a class="address" href="">201 Green Ridge Dr<br/>New Castle, PA 16105</a><!-- address -->
+									
+									<a class="tel" href="tel:724-652-4081">724-652-4081</a>
+									
+									<a class="get_directions" href="">Get Directions</a><!-- get_directions -->
+									
+								</div><!-- contact_info -->
+								
+								<div class="map">
+									
+									<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3007.5941665333276!2d-80.36567508458377!3d41.07786287929372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8833917f98c16d6b%3A0x81998c5d4c52c575!2s201+Green+Ridge+Rd%2C+New+Castle%2C+PA+16105!5e0!3m2!1sen!2sus!4v1484457883709" frameborder="0" style="border:0" allowfullscreen></iframe>
+								
+								</div><!-- map -->
+								
+							</div><!-- single_contact -->
+							
+							<div class="single_contact">
+								
+								<div class="contact_info">
+									
+									<span class="large_header">New Castle</span><!-- large_header -->
+									<a class="address" href="">201 Green Ridge Dr<br/>New Castle, PA 16105</a><!-- address -->
+									
+									<a class="tel" href="tel:724-652-4081">724-652-4081</a>
+									
+									<a class="get_directions" href="">Get Directions</a><!-- get_directions -->
+									
+								</div><!-- contact_info -->
+								
+								<div class="map">
+									
+									<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3007.5941665333276!2d-80.36567508458377!3d41.07786287929372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8833917f98c16d6b%3A0x81998c5d4c52c575!2s201+Green+Ridge+Rd%2C+New+Castle%2C+PA+16105!5e0!3m2!1sen!2sus!4v1484457883709" frameborder="0" style="border:0" allowfullscreen></iframe>
+								
+								</div><!-- map -->
+								
+							</div><!-- single_contact -->
+							
+							<div class="single_contact">
+								
+								<div class="contact_info">
+									
+									<span class="large_header">New Castle</span><!-- large_header -->
+									<a class="address" href="">201 Green Ridge Dr<br/>New Castle, PA 16105</a><!-- address -->
+									
+									<a class="tel" href="tel:724-652-4081">724-652-4081</a>
+									
+									<a class="get_directions" href="">Get Directions</a><!-- get_directions -->
+									
+								</div><!-- contact_info -->
+								
+								<div class="map">
+									
+									<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3007.5941665333276!2d-80.36567508458377!3d41.07786287929372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8833917f98c16d6b%3A0x81998c5d4c52c575!2s201+Green+Ridge+Rd%2C+New+Castle%2C+PA+16105!5e0!3m2!1sen!2sus!4v1484457883709" frameborder="0" style="border:0" allowfullscreen></iframe>
+								
+								</div><!-- map -->
+								
+							</div><!-- single_contact -->
+							
+							<div class="single_contact">
+								
+								<div class="contact_info">
+									
+									<span class="large_header">New Castle</span><!-- large_header -->
+									<a class="address" href="">201 Green Ridge Dr<br/>New Castle, PA 16105</a><!-- address -->
+									
+									<a class="tel" href="tel:724-652-4081">724-652-4081</a>
+									
+									<a class="get_directions" href="">Get Directions</a><!-- get_directions -->
+									
+								</div><!-- contact_info -->
+								
+								<div class="map">
+									
+									<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3007.5941665333276!2d-80.36567508458377!3d41.07786287929372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8833917f98c16d6b%3A0x81998c5d4c52c575!2s201+Green+Ridge+Rd%2C+New+Castle%2C+PA+16105!5e0!3m2!1sen!2sus!4v1484457883709" frameborder="0" style="border:0" allowfullscreen></iframe>
+								
+								</div><!-- map -->
+								
+							</div><!-- single_contact -->
+							
+							<div class="single_contact">
+								
+								<div class="contact_info">
+									
+									<span class="large_header">New Castle</span><!-- large_header -->
+									<a class="address" href="">201 Green Ridge Dr<br/>New Castle, PA 16105</a><!-- address -->
+									
+									<a class="tel" href="tel:724-652-4081">724-652-4081</a>
+									
+									<a class="get_directions" href="">Get Directions</a><!-- get_directions -->
+									
+								</div><!-- contact_info -->
+								
+								<div class="map">
+									
+									<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3007.5941665333276!2d-80.36567508458377!3d41.07786287929372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8833917f98c16d6b%3A0x81998c5d4c52c575!2s201+Green+Ridge+Rd%2C+New+Castle%2C+PA+16105!5e0!3m2!1sen!2sus!4v1484457883709" frameborder="0" style="border:0" allowfullscreen></iframe>
+								
+								</div><!-- map -->
+								
 							</div><!-- single_contact -->
 							
 						</div><!-- contact_wrapper -->
