fadcbc251aad957879fcf1829acade40fb326481
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-01-15 17:42:03 -0800

    push

---
+++ wp-content/themes/hartman/page-testimonials.php
@@ -0,0 +1,149 @@
+<?php 
+	
+/**
+ * Template Name: Testimonials
+ *
+ 
+ */
+	
+	get_header(); ?>
+
+	<?php include('banner.php');?>
+	
+
+		<div class="inner_main_no_sidebar">
+			
+			<div class="inner_container">
+				
+				
+				<div class="content">
+					
+					
+					<table class="blue_header_wrapper">
+					
+					<tr>
+						<td class="header_blue_line"></td><!-- blue_line -->
+						<td><h1><?php the_title();?></h1></td>
+						
+						
+					</tr>
+					
+					</table><!-- header_wrapper -->
+					
+					<div class="inner_content">
+					
+						<span class="my_excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</span>
+					
+										
+						<div class="testimonials_wrapper">
+							
+							<div class="testimonials_column">
+								
+								<div class="single_testimonial">
+									
+									<div class="stars">
+										
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+									
+									</div><!-- stars -->
+									
+									<span class="large_header">Lorem ipsum dolor sit amet</span><!-- large_header -->
+									
+									<p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.”</p>
+									
+									<span class="testimonial_name">Firstname N.</span><!-- testimonial_name -->
+									
+								</div><!-- single_testimonial -->
+								
+								<div class="single_testimonial">
+									
+									<div class="stars">
+										
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+									
+									</div><!-- stars -->
+									
+									<span class="large_header">Lorem ipsum dolor sit amet</span><!-- large_header -->
+									
+									<p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.”</p>
+									
+									<span class="testimonial_name">Firstname N.</span><!-- testimonial_name -->
+									
+								</div><!-- single_testimonial -->
+								
+							</div><!-- testimonials_column -->
+							
+							<div class="testimonials_column">
+								
+								<div class="single_testimonial">
+									
+									<div class="stars">
+										
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+									
+									</div><!-- stars -->
+									
+									<span class="large_header">Lorem ipsum dolor sit amet</span><!-- large_header -->
+									
+									<p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.”</p>
+									
+									<span class="testimonial_name">Firstname N.</span><!-- testimonial_name -->
+									
+								</div><!-- single_testimonial -->
+								
+								<div class="single_testimonial">
+									
+									<div class="stars">
+										
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+										<img class="star" src="<?php bloginfo('template_directory');?>/images/star.png"/>
+									
+									</div><!-- stars -->
+									
+									<span class="large_header">Lorem ipsum dolor sit amet</span><!-- large_header -->
+									
+									<p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+Duis aute irure dolor in reprehenderit in voluptate velit esse.”</p>
+									
+									<span class="testimonial_name">Firstname N.</span><!-- testimonial_name -->
+									
+								</div><!-- single_testimonial -->
+								
+							</div><!-- testimonials_column -->
+							
+						</div><!-- testimonials_wrapper -->
+					
+					
+					
+					</div><!-- inner_content -->
+					
+					
+				
+				
+				</div><!-- content -->
+			
+			</div><!-- inner_container -->
+			
+			
+		</div><!-- inner_main_with_sidebar -->
+
+			<?php  // get_template_part( 'loop', 'page' );?>
+
+			
+
+<?php get_footer(); ?>
