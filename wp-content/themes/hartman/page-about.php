1a04e501b57efdf0d6179931b298dbddd286e2d6
Garrett Cullen <garrettwcullen@gmail.com>
2017-01-17 10:33:30 -0800

    push

--- wp-content/themes/hartman/page-about.php
+++ wp-content/themes/hartman/page-about.php
@@ -32,21 +32,17 @@
 					
 					<div class="inner_content">
 					
-					<span class="my_excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</span>
+					<span class="my_excerpt"><?php the_field('excerpt');?></span>
 					
 					<div class="col">
 					
-					<h2>Lorem ipsum dolor sit amet</h2>
-
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam.</p>
-					
+					<?php the_field('coulumn_1');?>
+										
 					</div><!-- col -->
 					
 					<div class="col">
 					
-						<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat.</p>
-
-						<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde</p> 
+						<?php the_field('coulumn_2');?>
 					
 					</div><!-- col -->
 					
--- wp-content/themes/hartman/page-bio.php
+++ wp-content/themes/hartman/page-bio.php
@@ -31,11 +31,11 @@
 								
 								<td class="bio_cell">
 									
-									<img src="<?php bloginfo('template_directory');?>/images/internal_bio_hartman.jpg"/>
+									<img src="<?php the_field('attorney_image');?>"/>
 									
 									<div class="email_wrapper">
 									
-										<a class="email_attorney" href="">Email Dallas</a><!-- email_attorney -->
+										<a class="email_attorney" href="mailto:<?php the_field('attorney_email');?>"><?php the_field('email_button_verbaige');?></a><!-- email_attorney -->
 									
 									</div><!-- email_wrapper -->
 								
@@ -49,10 +49,10 @@
 									
 									<div class="sidebar_bio_content">
 							
-										<span class="bio_row">Title &nbsp;<span class="lightblue">|</span>&nbsp; <span class="description">Principal</span></span><!-- bio_row -->
-										<span class="bio_row">Location &nbsp;<span class="lightblue">|</span>&nbsp; <span class="description">New Castle, Pennsylvania</span></span><!-- bio_row -->
-										<span class="bio_row">Phone &nbsp;<span class="lightblue">|</span>&nbsp; <a class="description" href="tel:724-652-8380">724-652-8380</a></span><!-- bio_row -->
-										<span class="bio_row">Fax &nbsp;<span class="lightblue">|</span>&nbsp; <span class="description">724-652-8380</span></span><!-- bio_row -->
+										<span class="bio_row">Title &nbsp;<span class="lightblue">|</span>&nbsp; <span class="description"><?php the_field('attorney_title');?></span></span><!-- bio_row -->
+										<span class="bio_row">Location &nbsp;<span class="lightblue">|</span>&nbsp; <span class="description"><?php the_field('attorney_location');?></span></span><!-- bio_row -->
+										<span class="bio_row">Phone &nbsp;<span class="lightblue">|</span>&nbsp; <a class="description" href="tel:<?php the_field('attorney_phone_number');?>"><?php the_field('attorney_phone_number');?></a></span><!-- bio_row -->
+										<span class="bio_row">Fax &nbsp;<span class="lightblue">|</span>&nbsp; <span class="description"><?php the_field('attorney_fax_number');?></span></span><!-- bio_row -->
 							
 									</div><!-- sidebar_bio -->
 									
@@ -71,65 +71,10 @@
 						
 					<h1>Dallas W. Hartman</h1>
 					
-					<span class="my_excerpt">Dallas W. Hartman, principal at the law firm bearing his name, focuses his practice on personal injury, medical malpractice, legal malpractice and workers' compensation in Pennsylvania and Ohio.</span>
+					<span class="my_excerpt"><?php the_field('attorney_bio_excerpt');?></span>
 					
 					
-					<p>"Not all attorneys are equal. Therefore, the attorney you chose does make a difference." From highly complex litigation to routine auto cases Attorney Hartman believes each client deserves the attention that a corporate attorney gives a major corporation.</p>
-
-					<p>An honors graduate of Youngstown State University and the Dickinson School of Law, Hartman began practicing law in the Commonwealth of Pennsylvania in 1984. Dallas has been listed as one of the top 5% of lawyers in Pennsylvania by Super Lawyers Magazine and has been selected as one of the Top 50 Super Lawyers in Pittsburgh since 2012. He was proud to be included as one of the Top 100 Super Lawyers in Pennsylvania in 2013 and 2016. In addition, he was recognized by Newsweek magazine as a Leader in Product Liability Litigation. He is a lifetime member of the Million Dollar Advocates and the Multi-Million Dollar Advocates and is listed as one of the Top 100 Trial Lawyers.</p>
-
-					<p>"Maybe, my clients are more important to me because they are real people with real problems that all of us can understand," says Dallas. "Our legal system only works when clients are represented by attorneys with the knowledge, ability and resources to take a case to trial and through the appellate process."</p>
-					
-					
-					<h2>areas of practice</h2>
-					
-					<ul>
-						<li>Personal Injury</li>
-						<li>Animal Bites</li>
-						<li>Legal Malpractice</li>
-						<li> Medical Malpractice</li>
-						<li>Personal Injury</li>
-						<li>Animal Bites</li>
-						<li>Legal Malpractice</li>
-						<li> Medical Malpractice</li>
-						<li>Personal Injury</li>
-						<li>Animal Bites</li>
-						<li>Legal Malpractice</li>
-						<li> Medical Malpractice</li>
-					</ul>
-					
-					<h2>litigation percentage</h2>
-					
-					<p>60% of practice devoted to litigation.</p>
-
-					<h2>bar admissions</h2>
-					
-					<ul>
-						<li>Pennsylvania, 1984</li>
-						<li>Ohio, 1998</li>
-					</ul>
-
-					<h2>education</h2>
-					
-					<ul>
-						<li>The Dickinson School of Law, Carlisle, Pennsylvania</li>
-						<li>Youngstown State University, Youngstown, Ohio</li>
-					</ul>
-
-					<h2>honors and awards</h2>
-
-					<ul>
-						<li>Volunteer of the Year Award - Erie County, 2010 - Present</li>
-						<li>Pennsylvania Super Lawyers 2011, 2012, 2013, 2014, 2015, 2016</li>
-						<li>Newsweek Magazine National Leaders in Product Liability, April 9, 2012 - Present</li>
-						<li>Top 50 Pittsburgh Super Lawyers, 2012, 2013, 2014, 2015, 2016</li>
-						<li>Top 100 Trial Lawyers, 2012-2016</li>
-						<li>Top 100 Super Lawyers Pennsylvania, 2013, 2016</li>
-						<li>Lead Counsel Rated, Personal Injury</li>
-						<li>Lead Counsel Rated, Medical Malpractice</li>
-						<li>Lead Counsel Rated, Workers' Compensation	</li>
-					</ul>			
-					
+					<?php get_template_part( 'loop', 'page' );?>					
 					
 			
 				
